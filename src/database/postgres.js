06c925b3684ef73c595135ba52663f6f9b85bcf5
EduardoVSR1995 <eduardvitor7@gmail.com>
2022-10-16 02:19:23 -0300

    deploy for heroku

--- src/database/postgres.js
+++ src/database/postgres.js
@@ -7,6 +7,10 @@ dotenv.config();
 
 const connection = new Pool ({
             connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        
         });
         
 export default connection; 
